54b728156919afaffea930ad3c0ad0aca93a49ea
Topher Brown <topher200@gmail.com>
2015-11-16 23:45:07 -0500

    add main func to serve http files

---
+++ forty-thieves.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func handleResources(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Providing", r.URL.Path[1:])
+	http.ServeFile(w, r, r.URL.Path[1:])
+}
+
+func showHttp(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Root handling request:", r.URL.Path[1:])
+	http.ServeFile(w, r, "res/cards.html")
+}
+
+func main() {
+	http.HandleFunc("/res/", handleResources)
+	http.HandleFunc("/", showHttp)
+	http.ListenAndServe(":8080", nil)
+}
