cf7c3b7259edf0f2298569c37861191cee62e48e
Topher Brown <topher200@gmail.com>
2015-11-16 23:45:28 -0500

    add basic cards javascript
    This will probably be replaced with golang code (instead of javascript) - this
    is just for testing
    
    Taken from gist.github.com/mixtmeta/e4f60531fea20ba4b7c8

---
+++ res/viewmodel.js
@@ -0,0 +1,47 @@
+function CardGameViewModel(data)  {
+	var self = this;
+
+	self.cards = ko.observableArray(data.cards);
+	self.player1Cards= ko.observableArray(data.player1Cards);
+	self.player2Cards = ko.observableArray(data.player2Cards);		
+
+	self.player1Points = ko.computed(function() {
+		var points = 0;
+		self.player1Cards().forEach(function(card) {
+			points = points + app.cards[card.suit][card.rank];
+		});
+		return points;
+	});
+
+	self.player2Points = ko.computed(function() {
+		var points = 0;
+		self.player2Cards().forEach(function(card) {
+			points = points + app.cards[card.suit][card.rank];
+		});
+		return points;
+	});
+
+	self.remainingInDeck = ko.computed(function() {
+		return self.cards().length;
+	});
+
+	self.deal = function(hand, num) {
+		if(num > self.cards().length) {
+			num = self.cards().length;
+		}
+		for(var i = 0; i < num; i++) {
+			var card = self.cards.pop(); 
+			hand.push(card);
+		}
+	}
+
+	self.newGame = function() {
+		while(self.player1Cards().length > 0) {
+			self.cards.push(self.player1Cards.pop());
+		}
+		while(self.player2Cards().length > 0) {
+			self.cards.push(self.player2Cards.pop());
+		}
+		app.shuffle(self.cards());
+	}
+}
