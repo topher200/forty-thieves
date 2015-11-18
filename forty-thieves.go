834c115afd62ef20886ea017119fff08bbee43de
Topher Brown <topher200@gmail.com>
2015-11-18 03:01:57 -0500

    add json http callback for GameState

--- forty-thieves.go
+++ forty-thieves.go
@@ -1,13 +1,27 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+
+	"github.com/topher200/baseutil"
 )
+
+// Global var for GameState. We only support one game at a time.
+var gameState GameState
 
 func handleResources(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Providing", r.URL.Path[1:])
 	http.ServeFile(w, r, r.URL.Path[1:])
+}
+
+// handleStateRequest returns a json string of the current game state.
+func handleStateRequest(w http.ResponseWriter, r *http.Request) {
+	data, err := json.Marshal(&gameState)
+	baseutil.Check(err)
+	fmt.Println("providing json gamestate:", string(data))
+	fmt.Fprint(w, string(data))
 }
 
 func showHttp(w http.ResponseWriter, r *http.Request) {
@@ -16,8 +30,12 @@ func showHttp(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	fmt.Println(NewGame())
+	gameState = NewGame()
+
 	http.HandleFunc("/res/", handleResources)
+	http.HandleFunc("/state", handleStateRequest)
 	http.HandleFunc("/", showHttp)
-	// http.ListenAndServe(":8080", nil)
+
+	fmt.Println("Starting server...")
+	http.ListenAndServe(":8080", nil)
 }
