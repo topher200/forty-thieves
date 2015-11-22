4600094625bd29e05cb4b98bfc8550638dc5f279
Topher Brown <topher200@gmail.com>
2015-11-22 13:53:31 -0500

    test handleMoveRequest

--- forty-thieves_test.go
+++ forty-thieves_test.go
@@ -1,12 +1,24 @@
 package main
 
 import (
+	"bytes"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
+
+func TestHandleMoveRequest(t *testing.T) {
+	gameState = NewGame()
+	jsonStr := []byte(
+		`{"FromLocation": "tableau", "FromIndex": 0, "ToLocation": "tableau", "ToIndex": 1 }`)
+	req, err := http.NewRequest("POST", "", bytes.NewBuffer(jsonStr))
+	assert.Nil(t, err)
+	w := httptest.NewRecorder()
+	handleMoveRequest(w, req)
+	assert.Equal(t, 200, w.Code)
+}
 
 func TestShowHttp(t *testing.T) {
 	req, err := http.NewRequest("GET", "", nil)
