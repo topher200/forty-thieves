7481aa73ff186080521bf1340b4b3325e37b265c
Topher Brown <topher200@gmail.com>
2015-11-27 21:02:35 -0500

    save binarized game state to db

--- dal/game_state.go
+++ dal/game_state.go
@@ -1,6 +1,8 @@
 package dal
 
 import (
+	"bytes"
+	"encoding/gob"
 	"errors"
 	"fmt"
 
@@ -14,8 +16,8 @@ type GameStateDB struct {
 }
 
 type GameStateRow struct {
-	UserID          int64  `db:"user_id"`
-	SerializedState string `db:"serialized_state"`
+	UserID         int64  `db:"user_id"`
+	BinarizedState []byte `db:"binarized_state"`
 }
 
 func NewGameStateDB(db *sqlx.DB) *GameStateDB {
@@ -34,12 +36,15 @@ func GetGameState(userRow UserRow) *libgame.GameState {
 
 func (db *GameStateDB) SaveGameState(
 	tx *sqlx.Tx, userRow UserRow, gameState libgame.GameState) error {
-	data := make(map[string]interface{})
-	data["user_id"] = userRow.ID
-	// TODO(topher)
-	data["serialized_state"] = "asdf"
+	var binarizedState bytes.Buffer
+	encoder := gob.NewEncoder(&binarizedState)
+	encoder.Encode(gameState)
+	dataStruct := GameStateRow{userRow.ID, binarizedState.Bytes()}
 
-	insertResult, err := db.InsertIntoTable(tx, data)
+	dataMap := make(map[string]interface{})
+	dataMap["user_id"] = dataStruct.UserID
+	dataMap["binarized_state"] = dataStruct.BinarizedState
+	insertResult, err := db.InsertIntoTable(tx, dataMap)
 	if err != nil {
 		logrus.Warning("error saving game state:", err)
 		return err
--- migrations/1448668831_AddGameState.up.sql
+++ migrations/1448668831_AddGameState.up.sql
@@ -1,5 +1,5 @@
 CREATE TABLE game_state (
        id BIGSERIAL PRIMARY KEY NOT NULL,
        user_id INTEGER NOT NULL,
-       serialized_state TEXT NOT NULL
+       binarized_state BYTEA NOT NULL
 );
