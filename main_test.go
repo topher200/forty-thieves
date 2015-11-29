e7f6ada31a5c545d8c96c3eaeb0924e7df9c26ba
Topher Brown <topher200@gmail.com>
2015-11-28 19:02:14 -0500

    add the start of integration testing

---
+++ main_test.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/carbocation/interpose"
+	"github.com/stretchr/testify/assert"
+)
+
+func newApplicationForTesting(t *testing.T) *Application {
+	// TODO(topher): use testing db
+	app, err := NewApplication()
+	assert.Nil(t, err)
+	return app
+}
+
+func newMiddlewareForTesting(t *testing.T) *interpose.Middleware {
+	app := newApplicationForTesting(t)
+	middle, err := app.middlewareStruct()
+	assert.Nil(t, err)
+	return middle
+}
+
+func TestGetLogin(t *testing.T) {
+	middle := newMiddlewareForTesting(t)
+	r, err := http.NewRequest("GET", "/login", nil)
+	assert.Nil(t, err)
+	w := httptest.NewRecorder()
+	middle.ServeHTTP(w, r)
+	assert.Equal(t, 200, w.Code)
+	assert.NotEqual(t, "", w.Body.String())
+}
