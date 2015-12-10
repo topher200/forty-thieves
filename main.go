04fce932e7598d3ebc694534535bf7891af2aa86
Topher Brown <topher200@gmail.com>
2015-12-10 11:17:54 -0500

    nit

--- main.go
+++ main.go
@@ -86,7 +86,7 @@ func (app *Application) mux() *gorilla_mux.Router {
 	router.HandleFunc("/signup", handlers.PostSignup).Methods("POST").Name("/signup.Post")
 	router.HandleFunc("/login", handlers.GetLogin).Methods("GET").Name("/login.Get")
 	router.HandleFunc("/login", handlers.PostLogin).Methods("POST").Name("/login.Post")
-	router.HandleFunc("/logout", handlers.GetLogout).Methods("GET").Name("logout.Get")
+	router.HandleFunc("/logout", handlers.GetLogout).Methods("GET").Name("/logout.Get")
 
 	router.Handle(
 		"/users/{id:[0-9]+}",
