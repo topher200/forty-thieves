58e75cffd38f2f85910d18b05309075c61985647
Topher Brown <topher200@gmail.com>
2016-04-23 21:31:23 -0400

    Revert "clean up isMoveLegal logic"
    This reverts commit 3e59142b0a32db5316d2012a0e555913f8a0f2c8.
    
    I'm smarter than I thought - the if/else logic is like that because it's
    a true if/else

--- libgame/libgame.go
+++ libgame/libgame.go
@@ -83,32 +83,31 @@ func isMoveLegal(
 				cardBeingMoved)
 		}
 		// Empty tableaus are always OK moves
-	}
-
-	// Does the card we're moving "fit" on top of the card that's there?
-	destinationCard := toDeck.Cards[len(toDeck.Cards)-1]
-	if cardBeingMoved.Suit != destinationCard.Suit {
-		return fmt.Errorf("Illegal move - suits much match (%s on %s)",
-			cardBeingMoved, destinationCard)
-	}
-	switch toPile {
-	case tableau:
-		decrementedDestination, err := deck.Decrement(destinationCard.Face)
-		if err != nil {
-			return err
-		}
-		if decrementedDestination != cardBeingMoved.Face {
-			return fmt.Errorf("Illegal move - tableau cards must decrease (%s on %s)",
+	} else {
+		destinationCard := toDeck.Cards[len(toDeck.Cards)-1]
+		if cardBeingMoved.Suit != destinationCard.Suit {
+			return fmt.Errorf("Illegal move - suits much match (%s on %s)",
 				cardBeingMoved, destinationCard)
 		}
-	case foundation:
-		incrementedDestination, err := deck.Increment(destinationCard.Face)
-		if err != nil {
-			return err
-		}
-		if incrementedDestination != cardBeingMoved.Face {
-			return fmt.Errorf("Illegal move - foundation cards must increase (%s on %s)",
-				cardBeingMoved, destinationCard)
+		switch toPile {
+		case tableau:
+			decrementedDestination, err := deck.Decrement(destinationCard.Face)
+			if err != nil {
+				return err
+			}
+			if decrementedDestination != cardBeingMoved.Face {
+				return fmt.Errorf("Illegal move - tableau cards must decrease (%s on %s)",
+					cardBeingMoved, destinationCard)
+			}
+		case foundation:
+			incrementedDestination, err := deck.Increment(destinationCard.Face)
+			if err != nil {
+				return err
+			}
+			if incrementedDestination != cardBeingMoved.Face {
+				return fmt.Errorf("Illegal move - foundation cards must increase (%s on %s)",
+					cardBeingMoved, destinationCard)
+			}
 		}
 	}
 	return nil
