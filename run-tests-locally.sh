1f4bbd6eca9edf35197b603e978b65d19ef96cf5
Topher Brown <topher200@gmail.com>
2017-07-18 02:47:21 -0400

    remove old ./test.sh

--- run-tests-locally.sh
+++ run-tests-locally.sh
@@ -3,4 +3,10 @@
 # uses the docker postgres database
 export DSN="postgres://postgres@localhost:5432/forty-thieves?sslmode=disable"
 
-go test
+go test ./... "$@"
+
+retval=$?
+if [ $retval != 0 ]; then
+    echo "tests error! exit code: $retval"
+    exit $retval
+fi
--- test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-go test ./... "$@"
-
-retval=$?
-if [ $retval != 0 ]; then
-    echo "tests error! exit code: $retval"
-    exit $retval
-fi
