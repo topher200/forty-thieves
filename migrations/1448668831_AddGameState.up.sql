839931f449060db799931d3237f5543a97ea6436
Topher Brown <topher200@gmail.com>
2017-07-17 23:46:52 -0400

    add UUIDs for game state

--- migrations/1448668831_AddGameState.up.sql
+++ migrations/1448668831_AddGameState.up.sql
@@ -5,9 +5,10 @@ CREATE TABLE game (
 
 CREATE TABLE game_state (
        id BIGSERIAL PRIMARY KEY NOT NULL,
+       game_state_id UUID NOT NULL,
+       previous_game_state UUID,
        game_id INTEGER NOT NULL,
        move_num INTEGER NOT NULL,
        score INTEGER NOT NULL,
-       binarized_state BYTEA NOT NULL,
-       previous_game_state_id INTEGER
+       binarized_state BYTEA NOT NULL
 );
