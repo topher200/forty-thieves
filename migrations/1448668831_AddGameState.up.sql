1a3f34215b224c235418988a26f07b4503b8795a
Topher Brown <topher200@gmail.com>
2017-07-18 13:29:44 -0400

    add delete cascade to database migrator

--- migrations/1448668831_AddGameState.up.sql
+++ migrations/1448668831_AddGameState.up.sql
@@ -1,13 +1,13 @@
 CREATE TABLE game (
        id BIGSERIAL PRIMARY KEY NOT NULL,
-       user_id INTEGER REFERENCES users
+       user_id INTEGER REFERENCES users ON DELETE CASCADE
 );
 
 CREATE TABLE game_state (
        id BIGSERIAL PRIMARY KEY NOT NULL,
        game_state_id UUID NOT NULL,
-       previous_game_state UUID,
-       game_id INTEGER NOT NULL,
+       previous_game_state UUID REFERENCES game_state(game_state_id) ON DELETE CASCADE,
+       game_id INTEGER NOT NULL REFERENCES game ON DELETE CASCADE,
        move_num INTEGER NOT NULL,
        score INTEGER NOT NULL,
        binarized_state BYTEA NOT NULL
