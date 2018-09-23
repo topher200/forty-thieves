d84fb5560fac1a00c476e81b812ee97616c8aab1
Topher Brown <t.brown@wordstream.com>
2018-09-23 06:29:39 -0400

    fix up index to match our real query

--- migrations/1537682173_game-state-indeces.up.sql
+++ migrations/1537682173_game-state-indeces.up.sql
@@ -2,4 +2,4 @@
 CREATE UNIQUE INDEX ON game_state (game_id, binarized_state);
 
 -- index to make our common search action faster
-CREATE INDEX ON game_state (game_id, status) WHERE status != 'PROCESSED';
+CREATE INDEX ON game_state (game_id, status) WHERE status='UNPROCESSED';
